9a94548036a4e38cac17cffef4e6b35c88795b78
alobit21 <macmtavangu@gmail.com>
2024-08-15 10:20:25 +0300

    registration changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import Registration from "./registration";
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Registration from './components/Registration';
+// import NextRegistrationDetails from './components/NextRegistrationDetails'; // This should be your next registration page component
+// import Navbar from './components/Navbar';
+// import Login from './components/Login';
+// import Footer from './components/Footer';
+// import Home from './components/Home';
 function App() {
-  return(
-    <div>
-      <Registration/>
-    </div>
+  return (
+    <>
+    
+    <Router>
+      <div>
+      {/* <Navbar/> */}
+      </div>
+      <Routes>
+{/* 
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={<Home />} /> */}
+
+        <Route path="/" element={<Registration />} />
+        {/* <Route path="/nextRegistrationDetails" element={<NextRegistrationDetails />} /> */}
+      </Routes>
+      {/* <Footer/> */}
+    </Router>
+    </>
   );
 }
 
-export default App
+export default App;
